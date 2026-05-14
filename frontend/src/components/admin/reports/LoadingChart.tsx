a015bec369eeff1b77d26bcee46f42a08448e5b0
Alexey Zakharov <alexey.a.zakharov@gmail.com>
2026-05-14 09:39:20 +0300

    added todo

--- frontend/src/components/admin/reports/LoadingChart.tsx
+++ frontend/src/components/admin/reports/LoadingChart.tsx
@@ -30,7 +30,7 @@ export const LoadingChart: React.FC<LoadingChartProps> = ({
           <CardTitle>Загрузка тренировок</CardTitle>
         </CardHeader>
         <CardContent>
-          <Skeleton className="h-[300px] w-full" />
+          <Skeleton className="h-75 w-full" />
         </CardContent>
       </Card>
     );
@@ -42,7 +42,7 @@ export const LoadingChart: React.FC<LoadingChartProps> = ({
         <CardTitle>Загрузка тренировок</CardTitle>
       </CardHeader>
       <CardContent>
-        <div className="h-[300px] w-full">
+        <div className="h-75 w-full">
           <ResponsiveContainer width="100%" height="100%">
             <LineChart
               data={data}
